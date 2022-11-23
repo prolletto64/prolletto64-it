a280075956f817db9ae5e3f900d160b5c5940ddc
prolletto64 <poiluonzo@mail.com>
2022-11-23 12:39:06 +0100

    solved some issues with gh pages, react and tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
   );
 }
 
